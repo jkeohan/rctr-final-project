19d9a5c2494edb1af456f9f55cf587a72a969232
nefrob <nefrob.skyguy@gmail.com>
2021-09-11 22:47:31 -0600

    Implement exchange factory (creates/manages exchanges)

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,3 +1,38 @@
 pragma solidity ^0.8.7;
 
-contract Factory {}
+import "./interfaces/IFactory.sol";
+import "./Exchange.sol";
+
+contract Factory is IFactory {
+    //
+    mapping(address => address) public tokenToExchange;
+
+    function createExchange(address tokenAddress)
+        public
+        override
+        returns (address)
+    {
+        require(
+            tokenAddress != address(0),
+            "createExchange: tokenAddress is invalid"
+        );
+        require(
+            tokenToExchange[tokenAddress] == address(0),
+            "createExchange: tokenAddress already has an exchange"
+        );
+
+        Exchange exchange = new Exchange(tokenAddress);
+        tokenToExchange[tokenAddress] = address(exchange);
+
+        return address(exchange);
+    }
+
+    function getExchange(address tokenAddress)
+        public
+        view
+        override
+        returns (address)
+    {
+        return tokenToExchange[tokenAddress];
+    }
+}
--- contracts/Interfaces/IFactory.sol
+++ contracts/Interfaces/IFactory.sol
@@ -1,3 +1,17 @@
 pragma solidity ^0.8.7;
 
-interface IFactory {}
+interface IFactory {
+    /**
+     * @notice Creates a new ETH/Token exchange.
+     * @param tokenAddress The address of the Token to be traded.
+     * @return The address of the newly created exchange.
+     */
+    function createExchange(address tokenAddress) external returns (address);
+
+    /**
+     * @notice Gets exchange for the provided Token.
+     * @param tokenAddress The address of the Token exchange being searched for.
+     * @return The address of the newly exchange.
+     */
+    function getExchange(address tokenAddress) external view returns (address);
+}
